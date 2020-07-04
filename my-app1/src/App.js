3b69de4a31aa7e7a75b4ac9ff3a61b19e6ac5e36
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-04 11:52:13 +0300

    add class and func comps

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    clickHandler = () => {
+        console.log('Clicked')
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>Persons:</h1>
+                <Person name='John' age={33}/>
+
+                <h2>Event</h2>
+                <button onClick={this.clickHandler}>Click me</button>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ my-app1/src/Person.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Person({name, age}) {
+    return (<div>this is person, name: {name}, age: {age} </div>);
+}
+export default Person;
