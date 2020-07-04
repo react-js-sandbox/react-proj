57d7069741cf1db8796100158da2b48782c3e8d0
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-04 18:33:27 +0300

    add routing

---
+++ react-routing/src/Child1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Child1 extends Component {
+    render() {
+        return (<div>
+            <h2>Child1</h2>
+        </div>);
+    }
+}
+
+export default Child1;
---
+++ react-routing/src/Child2.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Child2 extends Component {
+    render() {
+        return (<div>
+            <h2>Child2</h2>
+        </div>);
+    }
+}
+
+export default Child2;
