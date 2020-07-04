5376f6463a3ba4b1719f51e077f72cac736e0496
Taras Hrynchuk <thrynchuk@vertafore.com>
2020-07-04 15:06:48 +0300

    add key to list

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-    state = {movies: [{name: 'Harry Potter', duration: 120}, {name: 'Harry Potter 2', duration: 130}]};
+    state = {movies: [{id: 1, name: 'Harry Potter', duration: 120}, {id: 2, name: 'Harry Potter 2', duration: 130}]};
 
     render() {
         let movieItems = this.state.movies.map((m, index) =>
-            <div><p
-            onClick={()=>{
-                let newMovies = [...this.state.movies];
-                newMovies.splice(index, 1);
-                this.setState({movies: newMovies});
-            }}>{index + 1}. {m.name}</p>{m.duration}</div>);
+            <div key={m.id}><p
+                onClick={() => {
+                    let newMovies = [...this.state.movies];
+                    newMovies.splice(index, 1);
+                    this.setState({movies: newMovies});
+                }}>{index + 1}. {m.name}</p>{m.duration}</div>);
         return (
             <div className="App">
                 {movieItems}
